a47e9ece60337ae917889ba583c9144068d088e0
dmydna <danimaydana9@gmail.com>
2025-12-04 10:42:49 -0300

    .

--- P1_Parciales/2c2023/R/solucion/ej1/ej1.c
+++ P1_Parciales/2c2023/R/solucion/ej1/ej1.c
@@ -1,16 +1,57 @@
 #include "ej1.h"
 
 string_proc_list* string_proc_list_create(void){
+	string_proc_list* list = (string_proc_list*) malloc(sizeof(string_proc_list))
+	list->first = NULL;
+	list->last = NULL;
+	return list;
 }
 
 string_proc_node* string_proc_node_create(uint8_t type, char* hash){
+	string_proc_node* node = (string_proc_node*) malloc(sizeof(string_proc_node))
+	node->next = NULL;
+	node->previous = NULL;
+	node->type = type;
+	node->hash = hash;
+	return node;
 }
 
 void string_proc_list_add_node(string_proc_list* list, uint8_t type, char* hash){
+	string_proc_node* prev = list->last;
+	string_proc_node* new_node = string_proc_node_create(type, hash);
+	new_node->next = NULL;
+	new_node->previous = prev;
+	prev->next = new_node;
+	list->last = new_node;
 }
 
 char* string_proc_list_concat(string_proc_list* list, uint8_t type , char* hash){
+	char* res = '\0'
+	while(node){
+		if(node->type == type){
+			res = str_concat(res, node->hash)
+		}
+		node = node->next;
+	}
+	return res;
 }
+
+
+char* init_list_concat(string_proc_list* list, uint8_t type){
+	string_proc_node* node = list->firts;
+	int longitud = 0;
+	while(node){
+		if(node->type == type){
+			longitud += strlen(node->hash)
+		}
+		node = node->next;
+	}
+	char* res = (char*) malloc(longitud + 1)
+	memset(res, 0, longitud + 1)
+	return res;
+}
+
+
 
 
 /** AUX FUNCTIONS **/
@@ -41,12 +82,12 @@ void string_proc_node_destroy(string_proc_node* node){
 
 char* str_concat(char* a, char* b) {
 	int len1 = strlen(a);
-    int len2 = strlen(b);
+	int len2 = strlen(b);
 	int totalLength = len1 + len2;
-    char *result = (char *)malloc(totalLength + 1); 
-    strcpy(result, a);
-    strcat(result, b);
-    return result;  
+	char *result = (char *)malloc(totalLength + 1); 
+	strcpy(result, a);
+	strcat(result, b);
+	return result;  
 }
 
 void string_proc_list_print(string_proc_list* list, FILE* file){
